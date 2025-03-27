56b328210c36530b817c3dda58e6daf2e14fcea6
aidosgal <agalimzhan928@gmail.com>
2025-03-27 21:07:38 +0500

    feat: initial commit

---
+++ internal/config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"flag"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Telegram Telegram `yaml:"telegram"`
+}
+
+type Telegram struct {
+	Api string `yaml:"api"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	return MustLoadByPath(path)
+}
+
+func MustLoadByPath(path string) *Config {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file does not exists: " + path)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("failed to read config: " + err.Error())
+	}
+
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
